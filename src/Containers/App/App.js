4de70beff1dce577f0d607494ccb1377d6b72ec7
fortanza <undertaker040880@gmail.com>
2021-06-02 15:00:00 +0200

    suppression taches

--- src/Containers/App/App.js
+++ src/Containers/App/App.js
@@ -1,27 +1,50 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from "react";
+import classes from "./App.module.css";
+import "./App.module.css";
 
-// Components 
+// Components
 import Task from "../../Components/Task/Task";
 
 function App() {
+  // States
+  const [tasks, setTasks] = useState([
+    {
+      task: "allez au marché",
+    },
+  ]);
+
+  // Méthodes
+
+  const removeClickHandler = (index) => {
+    const newtasks = [...tasks];
+    newtasks.splice(index, 1);
+    setTasks(newtasks);
+  };
+
+  let cartes = tasks.map((task, index) => {
+    return (
+      <Task
+        key={index}
+        task={task.task}
+        remove={() => removeClickHandler(index)}
+      ></Task>
+    );
+  });
+
+  // JSX
   return (
-    <div className="App">
+    <div className={classes.App}>
       <header>
         <span>TO-DO</span>
       </header>
 
-      <div className="add">
+      <div className={classes.add}>
         <form>
           <input type="text" placeholder="Que souhaitez-vous ajouter ?" />
-          <button type="submit">
-            Ajouter
-          </button>
+          <button type="submit">Ajouter</button>
         </form>
       </div>
-
-      
-
+      <div>{cartes}</div>
     </div>
   );
 }
