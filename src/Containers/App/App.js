6c98fbf5c6830b49dbba7a02fb1944650f7de6a9
fortanza <undertaker040880@gmail.com>
2021-06-02 18:34:03 +0200

    modif js

--- src/Containers/App/App.js
+++ src/Containers/App/App.js
@@ -6,12 +6,19 @@ import "./App.module.css";
 import Task from "../../Components/Task/Task";
 
 function App() {
+  
   // States
   const [tasks, setTasks] = useState([
     {
       task: "allez au marché",
     },
+    {
+      task: "Apprendre JS",
+    },
   ]);
+
+  const [transform, setTransform] = useState(false);
+
 
   // Méthodes
 
@@ -21,11 +28,18 @@ function App() {
     setTasks(newtasks);
   };
 
+  const strikeClickHandler = (index) => {
+    const newtasks = [...tasks];
+    newtasks[index].task = newtasks[index].task.strike();
+    setTasks(newtasks);
+  };
+
   let cartes = tasks.map((task, index) => {
     return (
       <Task
         key={index}
         task={task.task}
+        strike= {() => strikeClickHandler(index)}
         remove={() => removeClickHandler(index)}
       ></Task>
     );
@@ -44,7 +58,9 @@ function App() {
           <button type="submit">Ajouter</button>
         </form>
       </div>
-      <div>{cartes}</div>
+      <div transformed={transform}>
+        {cartes}
+      </div>
     </div>
   );
 }
