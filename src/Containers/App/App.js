21679f00f398df5b884700d8e99ccd6816fba062
fortanza <undertaker040880@gmail.com>
2021-06-08 16:18:14 +0200

    fin du projet

--- src/Containers/App/App.js
+++ src/Containers/App/App.js
@@ -8,20 +8,16 @@ import Task from "../../Components/Task/Task";
 function App() {
   
   // States
+
   const [tasks, setTasks] = useState([
-    {
-      content: "allez au marché",
-      done: false
-    },
-    {
-      content: "Apprendre JS",
-      done: false
-    },
+   
   ]);
+
+const [input, setInput] = useState('');
 
   // Méthodes
 
-  const removeClickHandler = (index) => {
+  const removeClickedHandler = (index) => {
     const newtasks = [...tasks];
     newtasks.splice(index, 1);
     setTasks(newtasks);
@@ -29,9 +25,25 @@ function App() {
 
   const strikeClickHandler = (index) => {
     const newtasks = [...tasks];
-    newtasks[index].task = newtasks[index].task.strike();
+    newtasks[index].done = !newtasks[index].done;
     setTasks(newtasks);
   };
+
+  const submittedTaskHandler = event => {
+    event.preventDefault();
+
+    const newTask = {
+      content: input,
+      done: false
+    }
+    setTasks([...tasks, newTask])
+    setInput('')
+  }
+
+const changedFormHandler = event => {
+  setInput(event.target.value);
+}  
+//Variables
 
   let tasksDisplayed = tasks.map((task, index) => {
     return (
@@ -40,11 +52,23 @@ function App() {
         done={task.done}
         task={task.content}
         strike= {() => strikeClickHandler(index)}
-        remove={() => removeClickHandler(index)}
+        remove={() => removeClickedHandler(index)}
       ></Task>
     );
   });
 
+  // let donedTasks = tasks.filter(task => task.done).map((filteredTask, index) => {
+  //   return (
+  //     <Task
+  //       key={index}
+  //       done={filteredTask.done}
+  //       task={filteredTask.content}
+  //       strike= {() => strikeClickHandler(index)}
+  //       remove={() => removeClickedHandler(index)}
+  //     ></Task>
+  //   );
+  // });
+  
   // JSX
   return (
     <div className={classes.App}>
@@ -52,13 +76,14 @@ function App() {
         <span>TO-DO</span>
       </header>
       <div className={classes.add}>
-        <form>
-          <input type="text" placeholder="Que souhaitez-vous ajouter ?" />
+        <form onSubmit={(e)=> submittedTaskHandler(e)}>
+          <input type="text" placeholder="Que souhaitez-vous ajouter ?" onChange={(e) => changedFormHandler(e)} value={input}/>
           <button type="submit">Ajouter</button>
         </form>
       </div>
       <div>
         {tasksDisplayed}
+        {/* {donedTasks} */}
       </div>
     </div>
   );
